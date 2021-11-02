943848ab5709c0a44c1af416f85f5d355f06c4bf
cine6m <cine99m@gmail.com>
2021-11-02 19:46:56 +0900

    스타듀밸리 협업 2주 포토폴리오

---
+++ openingScene.cpp
@@ -0,0 +1,45 @@
+#include "pch.h"
+#include "openingScene.h"
+
+
+HRESULT openingScene::init()
+{
+	_opening = MCIWndCreate(_hWnd, NULL, WS_CHILD | WS_VISIBLE | MCIWNDF_NOPLAYBAR, "source/intro.wmv");
+	MoveWindow(_opening, 0, 0, WINSIZEX, WINSIZEY, NULL);
+	MCIWndPlay(_opening);
+
+	SCENEMANAGER->SetVideoPlay(true);
+
+	return S_OK;
+}
+
+void openingScene::release()
+{
+}
+
+void openingScene::update()
+{
+	if (MCIWndGetLength(_opening) <= MCIWndGetPosition(_opening))
+	{
+		MCIWndDestroy(_opening);
+		SCENEMANAGER->SetVideoPlay(false);
+		SCENEMANAGER->changeScene("gameScene");
+	}
+
+	if (KEYMANAGER->isOnceKeyDown(VK_RETURN))
+	{
+
+		if (SCENEMANAGER->GetVideoPlay())
+		{
+			MCIWndDestroy(_opening);
+			SCENEMANAGER->SetVideoPlay(false);
+			SCENEMANAGER->changeScene("gameScene");
+
+		}
+	}
+}
+
+void openingScene::render()
+{
+	
+}
